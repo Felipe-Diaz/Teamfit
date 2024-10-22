b8dc1f779dfa2491060ebae29b47e50a73356ffc
Felipe-Diaz <fe.diazc@duocuc.cl>
2024-10-22 14:09:13 -0300

    Agregada dependencia a gunicorn

--- core/apis.py
+++ core/apis.py
@@ -7,7 +7,7 @@ import json
 from .models import Empleado
 
 URL_BASE = 'https://teamfit.openscorp.com/api/'
-API_KEY = 'TEAMFIT KEY' 
+API_KEY = 'RerPuUkAjjx7b2CDhw4XwDS30n3e5rH8qOh' 
 
 """
 **DESCRIPCIÓN INICIAL**\n
