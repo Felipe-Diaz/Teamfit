41251ec4342613cb07f5cc039d81695cafc5171c
Felipe-Diaz <fe.diazc@duocuc.cl>
2024-10-24 18:54:37 -0300

    Problemas de implementación - intento solución 1

--- Teamfit/settings.py
+++ Teamfit/settings.py
@@ -134,7 +134,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'core/static'),  # Define la ruta completa a tus archivos estáticos
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = path.join(BASE_DIR, 'media')
 
